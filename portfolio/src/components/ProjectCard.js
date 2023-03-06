85bc70343656cedb78cf04182d6ade01b4ab053e
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2023-03-06 22:36:39 +0300

    updated projectcard

--- portfolio/src/components/ProjectCard.js
+++ portfolio/src/components/ProjectCard.js
@@ -1,5 +1,5 @@
 import { Col,Button } from "react-bootstrap";
-export const ProjectCard = ({ project}) => {,
+export const ProjectCard = ({ project,deleteProject}) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
@@ -8,7 +8,7 @@ export const ProjectCard = ({ project}) => {,
           <h4>{project.title}</h4>
           <span>{project.description}</span>
         </div>
-        <Button variant="danger">Danger</Button>
+        <Button variant="danger" onClick={()=>{deleteProject(project.id)}}>Delete</Button>
       </div>
     </Col>
   )
